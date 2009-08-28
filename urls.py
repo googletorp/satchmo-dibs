99864a33f85503a8b85d8dfa56e7d5e6e984b55e
Jakob Torp Svendsen <jakob@revealit.dk>
2009-08-28 16:52:03 +0200

    Changed urls to fit DIBS for now.

--- urls.py
+++ urls.py
@@ -4,7 +4,7 @@ from livesettings import config_value, config_get_group
 config = config_get_group('PAYMENT_DUMMY')
 
 urlpatterns = patterns('',
-     (r'^$', 'payment.modules.dummy.views.pay_ship_info', {'SSL':config.SSL.value}, 'DUMMY_satchmo_checkout-step2'),
-     (r'^confirm/$', 'payment.modules.dummy.views.confirm_info', {'SSL':config.SSL.value}, 'DUMMY_satchmo_checkout-step3'),
-     (r'^success/$', 'payment.views.checkout.success', {'SSL':config.SSL.value}, 'DUMMY_satchmo_checkout-success'),
+     (r'^$', 'payment.modules.dibs.views.pay_ship_info', {'SSL':config.SSL.value}, 'DIBS_satchmo_checkout-step2'),
+     (r'^confirm/$', 'payment.modules.dibs.views.confirm_info', {'SSL':config.SSL.value}, 'DIBS_satchmo_checkout-step3'),
+     (r'^success/$', 'payment.views.checkout.success', {'SSL':config.SSL.value}, 'DIBS_satchmo_checkout-success'),
 )
